2475cea0da66846836d27f9b3ffd7541f5799889
mmilkov <mario.milkov26@gmail.com>
2025-05-19 11:17:13 +0200

    Klassen wurden fertiggestellt

--- TGMSEWFeedAnalyzer/AnalyzedText.java
+++ TGMSEWFeedAnalyzer/AnalyzedText.java
@@ -1,4 +1,72 @@
 package TGMSEWFeedAnalyzer;
 
-public class AnalyzedText {
+public class AnalyzedText implements Comparable<AnalyzedText> {
+    private String text;
+    private double sentiment;
+
+    public AnalyzedText(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public double getSentiment() {
+        return sentiment;
+    }
+
+    @Override
+    public int compareTo(AnalyzedText o) {
+        if (o == null)
+            return -1;
+        if (this.equals(o))
+            return 0;
+        int ret = this.text.compareTo(o.text);
+        if (ret != 0)
+            return ret;
+        ret = (int) this.sentiment - (int) o.sentiment;
+        if(ret != 0)
+            return ret;
+        return 0;
+    }
+
+    @Override
+    public int hashCode() {
+        int prime = 31;
+        int result = 1;
+        result = prime * result + ((text == null) ? 0 : text.hashCode());
+        result = prime * result + (int) sentiment;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (this.getClass() != obj.getClass()) {
+            return false;
+        }
+        AnalyzedText other = (AnalyzedText) obj;
+        if (text == null) {
+            if (other.text != null) {
+                return false;
+            }
+        } else if (!text.equals(other.text)) {
+            return false;
+        }
+        if (sentiment != other.sentiment) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Text: " + this.text + ", Sentiment: " + this.sentiment;
+    }
 }
--- TGMSEWFeedAnalyzer/Utility.java
+++ TGMSEWFeedAnalyzer/Utility.java
@@ -1,7 +1,40 @@
 package TGMSEWFeedAnalyzer;
 
+import java.io.*;
+import java.util.*;
+
 public class Utility {
     public double analyzeText(String text){
+        Set<String> stopWoerter = new HashSet<>();
+        Map<String, Double> vader = new HashMap<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader("./Resources/SmartStoplist.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                stopWoerter.add(line);
+            }
+        } catch (IOException e) {
+            System.err.println("Fehler: " + e.getMessage());
+        }
 
+        try (BufferedReader reader = new BufferedReader(new FileReader("./Resources/vader_lexicon.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split("\t");
+                if (parts.length >= 2) {
+                    vader.put(parts[0], Double.parseDouble(parts[1]));
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Fehler: " + e.getMessage());
+        }
+
+        double ergebnis = 0;
+        String[] woerter = text.split(" ");
+        for (String wort : woerter) {
+            if (!stopWoerter.contains(wort) && vader.containsKey(wort)) {
+                ergebnis += vader.get(wort);
+            }
+        }
+        return ergebnis;
     }
 }
